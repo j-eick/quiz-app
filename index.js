7de4d35a33ce0149e1f44d8553ab39bf95802e6b
Joschka Eickhoff <jeickhoff@posteo.de>
2022-07-04 21:55:00 +0200

    new css: submitButton.css

---
+++ index.js
@@ -0,0 +1,7 @@
+let submitButton = document.querySelector('[data-js="card_submitButton"]');
+
+submitButton.addEventListener("click", clickyDiClick);
+
+function clickyDiClick() {
+  console.log("click click");
+}
