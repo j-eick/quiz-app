7846f7b14f4eb4fc5d00eb9c98dae0774ce0d630
Joschka Eickhoff <jeickhoff@posteo.de>
2022-07-05 08:12:00 +0200

    show answer almost works

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 let submitButton = document.querySelector('[data-js="card_submitButton"]');
+console.log(submitButton);
+let cardAnswer = document.querySelector('[data-js="card__answer--hidden"]');
+console.log(cardAnswer);
 
 submitButton.addEventListener("click", clickyDiClick);
 
 function clickyDiClick() {
-  console.log("click click");
+  cardAnswer.classList.toggle("card__answer--hidden");
+  <></>;
 }
