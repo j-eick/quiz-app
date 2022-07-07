c46c05e95a44d7a5ef8f507d46176d1037543120
Joschka Eickhoff <jeickhoff@posteo.de>
2022-07-08 00:10:52 +0200

    bookmark fixed - finally

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ console.clear();
  */
 const submitButton = document.querySelector('[data-js="card__submitButton"]');
 const cardAnswer = document.querySelector("[data-js='card__answer']");
-const bookmarkBtn = document.querySelector('[data-js="bookmark--active"]');
+const bookmarkBtn = document.querySelector('[data-js="bookmarkButton"]');
+const bookmark_show = document.querySelector('[data-js="bookmark--active"]');
 
 /**
  * CONSOLE TEST
@@ -13,18 +14,20 @@ const bookmarkBtn = document.querySelector('[data-js="bookmark--active"]');
 console.log(submitButton);
 console.log(cardAnswer);
 console.log(bookmarkBtn);
+console.log(bookmark_show);
 
 /**
  * BUTTONS
  */
 // BOOKMARK
-bookmarkBtn.addEventListener("click", function activate() {
-  bookmarkBtn.classList.toggle(".button_bookmark--show");
+bookmarkBtn.addEventListener("click", () => {
+  console.log("bla");
+  bookmark_show.classList.toggle("button__bookmark--show");
 });
 
 // SHOW ANSWER
-submitButton.addEventListener("click", function showAnswer() {
-  cardAnswer.classList.toggle(".card__answer--hidden");
+submitButton.addEventListener("click", () => {
+  cardAnswer.classList.toggle("card__answer--hidden");
 
   // TODO - add line that changes buttontext to
   //        "hide answer"
