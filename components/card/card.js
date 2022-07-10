d0644a28a92ae33a657b55219fa021fb2a34628f
Joschka Eickhoff <jeickhoff@posteo.de>
2022-07-10 23:20:57 +0200

    adding cards in js

--- components/card/card.js
+++ components/card/card.js
@@ -4,4 +4,34 @@ export default function Card() {
   const card = document.querySelector("[data-js='card']");
 
   console.log(card);
+
+  const cards = [
+    {
+      question: "What did Morpheus say?",
+      Answer: "Red or blue pill?",
+      Tags: ["a", "b", "c", "d"],
+    },
+    {
+      question: "What did Niobe say?",
+      Answer: "Something important!",
+      Tags: ["a", "b", "c", "d"],
+    },
+    {
+      question: "What did Cypher say?",
+      Answer: "Ignorance is bliss!",
+      Tags: ["a", "b", "c", "d"],
+    },
+    {
+      question: "What did Agent Smith say?",
+      Answer: "Mr. Anderson!",
+      Tags: ["a", "b", "c", "d"],
+    },
+  ];
+
+  /**
+   * CARD Reneration
+   */
+  cards.forEach((card) => {
+    const quizCards = document.create(article);
+  });
 }
