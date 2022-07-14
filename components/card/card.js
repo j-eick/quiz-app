20da4dfe79209c4cd862e05c738c48056dc966a3
Joschka Eickhoff <jeickhoff@posteo.de>
2022-07-14 15:23:30 +0200

    add linting

--- components/card/card.js
+++ components/card/card.js
@@ -7,24 +7,24 @@ export default function Card() {
 
   const cards = [
     {
-      question: "What did Morpheus say?",
-      answer: "Red or blue pill?",
-      tags: ["a", "b", "c", "d"],
+      question: 'What did Morpheus say?',
+      answer: 'Red or blue pill?',
+      tags: ['a', 'b', 'c', 'd'],
     },
     {
-      question: "What did Niobe say?",
-      answer: "Something important!",
-      tags: ["a", "b", "c", "d"],
+      question: 'What did Niobe say?',
+      answer: 'Something important!',
+      tags: ['a', 'b', 'c', 'd'],
     },
     {
-      question: "What did Cypher say?",
-      answer: "Ignorance is bliss!",
-      tags: ["a", "b", "c", "d"],
+      question: 'What did Cypher say?',
+      answer: 'Ignorance is bliss!',
+      tags: ['a', 'b', 'c', 'd'],
     },
     {
-      question: "What did Agent Smith say?",
-      answer: "Mr. Anderson!",
-      tags: ["a", "b", "c", "d"],
+      question: 'What did Agent Smith say?',
+      answer: 'Mr. Anderson!',
+      tags: ['a', 'b', 'c', 'd'],
     },
   ];
 
@@ -36,45 +36,45 @@ export default function Card() {
   /**
    * CARD Reneration
    */
-  cards.forEach((card) => {
+  cards.forEach(card => {
     // Card
-    const quizCard = document.createElement("article");
-    quizCard.classList.add("card", "card__text", "p");
+    const quizCard = document.createElement('article');
+    quizCard.classList.add('card', 'card__text', 'p');
 
     // Bookmark
-    const bookmark = document.createElement("button");
+    const bookmark = document.createElement('button');
     bookmark.classList.add(
-      "button",
-      "button__bookmark--active",
-      "button__bookmark--inactive",
-      "button__bookmark--show"
+      'button',
+      'button__bookmark--active',
+      'button__bookmark--inactive',
+      'button__bookmark--show'
     );
 
     // Question
-    const questionText = document.createElement("p");
-    questionText.classList.add("card__text");
+    const questionText = document.createElement('p');
+    questionText.classList.add('card__text');
     questionText.innerText = card.question;
 
     // AnswerButton
-    const answerButton = document.createElement("button");
-    answerButton.classList.add("card__submit", "card__submitButton");
-    answerButton.innerText = "Show Answer";
-    answerButton.addEventListener("click", () => {
-      answerText.classList.toggle("card__answer--hidden");
+    const answerButton = document.createElement('button');
+    answerButton.classList.add('card__submit', 'card__submitButton');
+    answerButton.innerText = 'Show Answer';
+    answerButton.addEventListener('click', () => {
+      answerText.classList.toggle('card__answer--hidden');
     });
 
     // TipButton
-    const tipButton = document.createElement("button");
-    tipButton.classList.add("card__submit", "card__tipButton");
-    tipButton.innerText = "Show tip";
+    const tipButton = document.createElement('button');
+    tipButton.classList.add('card__submit', 'card__tipButton');
+    tipButton.innerText = 'Show tip';
 
     // Answer
-    const answerText = document.createElement("p");
+    const answerText = document.createElement('p');
     answerText.classList.add(
-      "card__answer",
-      "card__answer>p",
-      "answer_left",
-      "answer_right"
+      'card__answer',
+      'card__answer>p',
+      'answer_left',
+      'answer_right'
     );
 
     // loop over ul
